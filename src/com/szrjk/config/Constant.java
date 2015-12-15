bebff54eb0c5aefe05d1435b0b4e64e414cf6492
denggm <denggm@szrjk.com>
2015-12-15 14:42:44 +0800

    constant

--- src/com/szrjk/config/Constant.java
+++ src/com/szrjk/config/Constant.java
@@ -31,10 +31,10 @@ public class Constant
 	//外网环境
 //	public static  String RQEUEST_URL = "http://120.236.165.150:8888/qry";
 //	 测试环境
-//	 public static String RQEUEST_URL = "http://192.168.1.82:8888/qry";
+	 public static String RQEUEST_URL = "http://192.168.1.82:8888/qry";
 	//UAT环境
 //	public static String RQEUEST_URL = "http://112.74.109.173:8888/qry";
-	public static String RQEUEST_URL = "http://120.25.134.160:1127/qry";
+//	public static String RQEUEST_URL = "http://120.25.134.160:1127/qry";
 
 
 

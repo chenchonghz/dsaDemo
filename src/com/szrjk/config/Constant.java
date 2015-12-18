8809406895f31af06f1b427f36b9f3b4ab436352
denggm <denggm@szrjk.com>
2015-12-18 14:55:29 +0800

    11

--- src/com/szrjk/config/Constant.java
+++ src/com/szrjk/config/Constant.java
@@ -27,11 +27,11 @@ public class Constant
 	public static final String PRIVATEKEY = "szrjk2015";
 	// 寮�鍙戠幆澧�
 
-	public static String RQEUEST_URL = "http://192.168.1.71:8888/qry";
+//	public static String RQEUEST_URL = "http://192.168.1.71:8888/qry";
 	//澶栫綉鐜
 //	public static  String RQEUEST_URL = "http://120.236.165.150:8888/qry";
 //	 娴嬭瘯鐜
-//	 public static String RQEUEST_URL = "http://192.168.1.82:8888/qry";
+	 public static String RQEUEST_URL = "http://192.168.1.82:8888/qry";
 	//UAT鐜
 //	public static String RQEUEST_URL = "http://112.74.109.173:8888/qry";
 //	public static String RQEUEST_URL = "http://120.25.134.160:1127/qry";

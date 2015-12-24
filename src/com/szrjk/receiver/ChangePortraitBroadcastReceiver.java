5045f7fb7a50bfb86f72871f3b32af1048974959
wangkz <wangkz@digi123.cn>
2015-12-24 15:38:00 +0800

    更多数据更新

--- src/com/szrjk/receiver/ChangePortraitBroadcastReceiver.java
+++ src/com/szrjk/receiver/ChangePortraitBroadcastReceiver.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 
 import com.szrjk.config.ConstantUser;
 import com.szrjk.dhome.DHomeApplication;
+import com.szrjk.dhome.MainActivity;
 import com.szrjk.dhome.SelfActivity;
 
 public class ChangePortraitBroadcastReceiver extends BroadcastReceiver {
@@ -13,6 +14,9 @@ public class ChangePortraitBroadcastReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		String userfaceUrl = intent.getStringExtra("userfaceUrl");
 		ConstantUser.getUserInfo().setUserFaceUrl(userfaceUrl);
+		MainActivity mainActivity=((DHomeApplication) context
+				.getApplicationContext()).mainActivity;
+		mainActivity.setDataChange();
 		SelfActivity selfActivity = ((DHomeApplication) context
 				.getApplicationContext()).selfActivity;
 		selfActivity.getUserHpInfo(ConstantUser.getUserInfo().getUserSeqId());
